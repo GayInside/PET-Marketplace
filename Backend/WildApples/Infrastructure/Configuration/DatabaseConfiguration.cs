70378036ed0da72b35b7ef7d4a2b220f19b1eb5d
a.alexandrglazko <alexandrglazko@mail.ru>
2024-05-02 18:27:12 +0300

    WildApples-2: Added postrgres implementation using entity framework. Made first migration.

---
+++ Backend/WildApples/Infrastructure/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.DataBaseContext;
+using Infrastructure.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Configuration
+{
+    public static class DatabaseConfiguration
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            var connectionString = EnvFetcher.GetRequiredEnvVariable("CONNECTION_STRING");
+            services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connectionString));
+
+            return services;
+        }
+    }
+}
---
+++ Backend/WildApples/Infrastructure/Helpers/EnvFetcher.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Helpers
+{
+    internal static class EnvFetcher
+    {
+        private static string? GetEnvVariable(string variableName)
+        {
+            var envVar = Environment.GetEnvironmentVariable(variableName);
+            return envVar;
+        }
+
+        public static string GetRequiredEnvVariable(string variableName)
+        {
+            return GetEnvVariable(variableName) ?? throw new Exception($"No {variableName} provided");
+        }
+    }
+}
