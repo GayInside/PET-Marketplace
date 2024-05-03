d5dd4299ce7a5a8b4fbf45152680b8e2dda51841
a.alexandrglazko <alexandrglazko@mail.ru>
2024-05-03 15:48:19 +0300

    WildApples-5: Add user roles

---
+++ Backend/WildApples/Domain/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Utils;
+
+namespace Domain.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; set; } = RolesScheme.DEFAULT_USER_ROLE;
+
+        public virtual IEnumerable<User>? UsersWithRole { get; set; }
+    }
+}
